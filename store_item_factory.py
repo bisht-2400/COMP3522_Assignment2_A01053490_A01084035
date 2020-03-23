a46e99bed3a1123f92d2b21cde3f5aa05c36c303
YASHSVI GIRDHAR <45160510+YashsviG@users.noreply.github.com>
2020-03-22 22:50:35 -0700

    Updated the Store Item Factory.

--- store_item_factory.py
+++ store_item_factory.py
@@ -13,50 +13,119 @@ from toy import RobotBunny
 
 
 class StoreItemFactory(ABC):
+    """
+    Factory for the StoreItem.
+    """
     @abstractmethod
     def create_toy(self):
+        """
+        Creates Items: Toy
+        :return:
+        """
         pass
 
     @abstractmethod
     def create_stuff_animal(self):
+        """
+        Creates Items: Stuffed Animals
+        :return:
+        """
         pass
 
     @abstractmethod
     def create_candy(self):
+        """
+        Creates Items: Candy
+        :return:
+        """
         pass
 
 
 class ChristmasItemFactory(StoreItemFactory):
+    """
+    Holiday: Christmas Factory class.
+    """
 
     def create_candy(self, **kwargs):
+        """
+        Creates the candy.
+        :param kwargs: Attributes to initialize the candy.
+        :return: CandyCane Object.
+        """
         return CandyCane(**kwargs)
 
     def create_stuff_animal(self, **kwargs):
+        """
+        Creates the Stuffed Animal.
+        :param kwargs: Attributes to initialize the Stuffed Animal.
+        :return: Reindeer Object.
+        """
         return Reindeer(**kwargs)
 
     def create_toy(self, **kwargs):
+        """
+        Creates the Toy.
+        :param kwargs: Attributes to initialize the toy.
+        :return: SantaWorkshop Object.
+        """
         return SantaWorkshop(**kwargs)
 
 
 class HalloweenItemFactory(StoreItemFactory):
+    """
+    Holiday: Halloween Factory class.
+    """
 
     def create_candy(self, **kwargs):
+        """
+        Creates the candy.
+        :param kwargs: Attributes to initialize the candy.
+        :return: PumpkinCaramelToffee Object.
+        """
         return PumpkinCaramelToffee(**kwargs)
 
     def create_stuff_animal(self, **kwargs):
+        """
+        Creates the Stuffed Animal.
+        :param kwargs: Attributes to initialize the Stuffed Animal.
+        :return: DancingSkeleton Object.
+        """
         return DancingSkeleton(**kwargs)
 
     def create_toy(self, **kwargs):
+        """
+        Creates the Toy.
+        :param kwargs: Attributes to initialize the toy.
+        :return: RCSpider Object.
+        """
         return RCSpider(**kwargs)
 
 
 class EasterItemFactory(StoreItemFactory):
+    """
+    Holiday: Easter Factory class.
+    """
 
     def create_candy(self, **kwargs):
+        """
+        Creates the candy.
+        :param kwargs: Attributes to initialize the candy.
+        :return: CremeEgg Object.
+        """
         return CremeEgg(**kwargs)
 
     def create_stuff_animal(self, **kwargs):
+        """
+        Creates the Stuffed Animal.
+        :param kwargs: Attributes to initialize the Stuffed Animal.
+        :return: EasterBunny Object.
+        """
         return EasterBunny(**kwargs)
 
     def create_toy(self, **kwargs):
+        """
+        Creates the Toy.
+        :param kwargs: Attributes to initialize the toy.
+        :return: RobotBunny Object.
+        """
         return RobotBunny(**kwargs)
