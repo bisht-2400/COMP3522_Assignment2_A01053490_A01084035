fe969364be3cc19459df6cbb23b58c0cddc94211
YASHSVI GIRDHAR <45160510+YashsviG@users.noreply.github.com>
2020-03-22 22:51:39 -0700

    Updated the Stuffing enum.

--- stuffing.py
+++ stuffing.py
@@ -2,6 +2,9 @@ from enum import Enum, auto
 
 
 class Stuffing(Enum):
+    """
+    Stuffing Enum: for the different available stuffing.
+    """
     POLYESTER_FIBERFILL = auto()
     WOOL = auto()
 
