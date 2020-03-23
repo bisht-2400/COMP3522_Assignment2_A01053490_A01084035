6d7f534199324cc807c8d67ef4c85a1f2b3ae723
Kshitiz Bisht <kbisht@my.bcit.ca>
2020-03-22 17:30:40 -0700

    Assignment_2 order fields checked

--- process_web_orders.py
+++ process_web_orders.py
@@ -3,12 +3,15 @@ import factory_pattern_constructors
 
 
 class Order:
-    def __init__(self, factory, order_number, item, quantity, **kwargs):
+    def __init__(self, factory, is_valid, order_number, item, quantity, **kwargs):
         self.order_num = order_number
+        self.is_valid = is_valid
         self.factory = factory
         self.item = item
         self.quantity = quantity
         self.attributes = {key: kwargs[key] for key in kwargs if pd.notna(kwargs[key])}
+        # for key in kwargs:
+        #     print(key, type(kwargs[key]))
 
 
 class FactoryMapper:
@@ -24,14 +27,28 @@ class FactoryMapper:
 
 class ProcessWebOrders:
     @staticmethod
+    def helper_check_y_or_n(field):
+        if field == "Y" or "N" or "nan":
+            return True
+        return False
+
+    @staticmethod
     def generate_orders():
         excel_file = "C:\\Users\\Kshitiz Bisht\\Desktop\\BCIT\\Term-3\\ObjectOriented\\Assignment_2\\orders.xlsx"
         for row in pd.read_excel(excel_file).iterrows():
             factory = FactoryMapper.factory_mapper(row[1]['holiday'])()
-            order = Order(factory=factory, **row[1])
+            is_valid = ProcessWebOrders.check_valid_order(row[1])
+            order = Order(factory=factory, is_valid=is_valid, **row[1])
             del order.attributes['holiday']
             yield order
 
-
-for i in ProcessWebOrders.generate_orders():
-    print(i.order_num)
+    @staticmethod
+    def check_valid_order(order):
+        if ProcessWebOrders.helper_check_y_or_n(order['has_batteries']) and ProcessWebOrders.helper_check_y_or_n(order['has_glow']) and ProcessWebOrders.helper_check_y_or_n(order['has_lactose']) and ProcessWebOrders.helper_check_y_or_n(order['has_nuts']):
+            if order['variety'] == 'Sea Salt' or 'Regular' or 'nan':
+                if order['stuffing'] == 'Polyester Fiberfill' or 'Wool' or 'nan':
+                    if order['fabric'] == 'Linen' or 'Cotton' or 'Acrylic' or 'nan':
+                        if order['size'] == 'Small' or 'Medium' or 'Large' or 'nan':
+                            return True
+        else:
+            return False
