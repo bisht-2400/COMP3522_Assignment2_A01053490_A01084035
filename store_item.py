1273e88f08964b6a89a21c7c1674f27208f2a800
YASHSVI GIRDHAR <45160510+YashsviG@users.noreply.github.com>
2020-03-22 22:50:00 -0700

    Updated the Store Item class.

--- store_item.py
+++ store_item.py
@@ -1,5 +1,14 @@
 class StoreItem:
+    """
+    Base class for the items in the Store.
+    """
     def __init__(self, name, description, product_id):
+        """
+        Initializer for the StoreItem: Common Attributes.
+        :param name: String
+        :param description: String
+        :param product_id: String
+        """
         self._name = name
         self._description = description
         self._product_id = product_id
