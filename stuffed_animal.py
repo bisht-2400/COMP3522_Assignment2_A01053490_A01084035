e1dd4aff224ebf1fdcc048b3b5c8a16685d8c111
YASHSVI GIRDHAR <45160510+YashsviG@users.noreply.github.com>
2020-03-22 22:51:12 -0700

    Updated the Stuffed Animal class.

--- stuffed_animal.py
+++ stuffed_animal.py
@@ -1,11 +1,22 @@
 from store_item import StoreItem
 
-from stuffing import Stuffing
-from fabric import Fabric
+from Stuffing import Stuffing
+from Fabric import Fabric
 
 
 class StuffedAnimal(StoreItem):
+    """
+    Stuffed Animal item: Inherits from StoreItem.
+    """
+
     def __init__(self, stuffing, size, fabric, **kwargs):
+        """
+        Initializer for the Stuffed Animal class.
+        :param stuffing: String
+        :param size: String
+        :param fabric: String
+        :param kwargs: Other StoreItem Attributes.
+        """
         super().__init__(**kwargs)
         self._stuffing = stuffing
         self._size = size
@@ -13,16 +24,41 @@ class StuffedAnimal(StoreItem):
 
 
 class Reindeer(StuffedAnimal):
+    """
+    Reindeer: Inherits from Stuffed Animal class.
+    """
+
     def __init__(self, **kwargs):
+        """
+        Initializer for the Reindeer class.
+        :param kwargs: Other StoreItem Attributes.
+        """
         super().__init__(stuffing=Stuffing.WOOL, fabric=Fabric.COTTON, **kwargs)
 
 
 class DancingSkeleton(StuffedAnimal):
+    """
+    DancingSkeleton: Inherits from Stuffed Animal class.
+    """
+
     def __init__(self, **kwargs):
+        """
+        Initializer for the DancingSkeleton class.
+        :param kwargs: Other StoreItem Attributes.
+        """
         super().__init__(stuffing=Stuffing.POLYESTER_FIBERFILL, fabric=Fabric.ACRYLIC, **kwargs)
 
 
 class EasterBunny(StuffedAnimal):
+    """
+    EasterBunny: Inherits from the Stuffed Animal class.
+    """
+
     def __init__(self, color, **kwargs):
+        """
+        Initializer for the EasterBunny class.
+        :param color: String
+        :param kwargs: Other StoreItem Attributes.
+        """
         super().__init__(stuffing=Stuffing.POLYESTER_FIBERFILL, fabric=Fabric.LINEN, **kwargs)
         self.color = color
