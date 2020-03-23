67f2f17d5f3b80eb45f27470376da1439e7a674a
YASHSVI GIRDHAR <45160510+YashsviG@users.noreply.github.com>
2020-03-22 19:43:11 -0700

    Delete item.py

--- item.py
+++
@@ -1,9 +0,0 @@
-class Item:
-
-    def __init__(self, name, description, product_id):
-        self._description = description
-        self._product_id = product_id
-        self._name = name
-
-    def getName(self):
-        return self._name
